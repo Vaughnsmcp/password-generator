c0310ce265d9490bd643b8bf748d1ca70fcf83ad
Vaughn McPherson <Vaughnsmcp@gmail.com>
2020-02-04 17:24:16 -0800

    updated readme and style

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # password-generator
+
+Input the amount of characters you'd like in your password (between 8 and 128), what types of characters to use (special, numerical, uppercase, lower case). Generate password.
+
+
+## Getting Started
+
+Simply enter the number of characters you'd like, the type of characters (s=special character, n=number, u=uppercase, and l=lowercase). Click the Generate button, and your password apears in the text box. 
+
+
+## Authors
+
+Vaughn McPherson
+
+
+## Acknowledgments
+
+* Tip of the hat to both Frantz and Elrey! Frantz helped me get the generator to function properly, Elrey helped me get an actual page instead of just pop ups. 
--- index.js
+++ index.js
@@ -5,67 +5,71 @@ var passwordCriteriaBoolSpecial = false;
 var passwordCriteriaBoolNumerical = false;
 var passwordCriteriaBoolUpper = false;
 var passwordCriteriaBoolLower = false;
+var buttonElement=document.getElementById("button").addEventListener("click", generate);
 
+function generate(){
+    while (passwordLength !== null) {
 
-while (passwordLength !== null) {
-
-    passwordLength = prompt("Password Length is between 8-128 characters");
-    passwordLength = parseInt(passwordLength);
-
-    //exit loop
-
-    if (isNaN(passwordLength)) {
-        alert("Please enter a valid number");
-
+        passwordLength = prompt("Password Length is between 8-128 characters");
+        passwordLength = parseInt(passwordLength);
+    
+        //exit loop
+    
+        if (isNaN(passwordLength)) {
+            alert("Please enter a valid number");
+    
+        }
+        if (passwordLength < 8 || passwordLength > 128) {
+            alert("Please enter between 8 and 128 charaters");
+        }
+        if (passwordLength > 8 || passwordLength < 128) {
+            break;
+        }
     }
-    if (passwordLength < 8 || passwordLength > 128) {
-        alert("Please enter between 8 and 128 charaters");
+    while (passwordCriteria !== null) {
+    
+    
+    
+    
+        passwordCriteria = prompt("Characters must inlude special, numeric, lowercase, and uppercase. Enter 'S' for Special, 'N' for number, 'L' for lowercase, 'U' for uppercase.");
+        if (passwordCriteria !== null) { passwordCriteriaArr = passwordCriteria.split(""); }
+    
+    
+        for (var i = 0; i < passwordCriteriaArr.length; i++) {
+            if (passwordCriteriaArr[i].includes('s')) {
+                passwordCriteriaBoolSpecial = true;
+    
+            }
+            if (passwordCriteriaArr[i].includes('n')) {
+                passwordCriteriaBoolNumerical = true;
+    
+            }
+            if (passwordCriteriaArr[i].includes('l')) {
+                passwordCriteriaBoolLower = true;
+            }
+            if (passwordCriteriaArr[i].includes('u')) {
+                passwordCriteriaBoolUpper = true;
+            }
+        }
+    
+        if (passwordCriteriaBoolSpecial === true ||
+            passwordCriteriaBoolNumerical === true ||
+            passwordCriteriaBoolUpper === true ||
+            passwordCriteriaBoolLower === true) {
+            break;
+        }
+        else {
+            alert("enter a valid password");
+    
+        }
+    
+    
     }
-    if (passwordLength > 8 || passwordLength < 128) {
-        break;
-    }
+    
+    generatePassword(passwordLength, passwordCriteriaBoolSpecial, passwordCriteriaBoolNumerical, passwordCriteriaBoolLower, passwordCriteriaBoolUpper);
+
 }
-while (passwordCriteria !== null) {
 
-
-
-
-    passwordCriteria = prompt("Characters must inlude special, numeric, lowercase, and uppercase");
-    if (passwordCriteria !== null) { passwordCriteriaArr = passwordCriteria.split(""); }
-
-
-    for (var i = 0; i < passwordCriteriaArr.length; i++) {
-        if (passwordCriteriaArr[i].includes('s')) {
-            passwordCriteriaBoolSpecial = true;
-
-        }
-        if (passwordCriteriaArr[i].includes('n')) {
-            passwordCriteriaBoolNumerical = true;
-
-        }
-        if (passwordCriteriaArr[i].includes('l')) {
-            passwordCriteriaBoolLower = true;
-        }
-        if (passwordCriteriaArr[i].includes('u')) {
-            passwordCriteriaBoolUpper = true;
-        }
-    }
-
-    if (passwordCriteriaBoolSpecial === true ||
-        passwordCriteriaBoolNumerical === true ||
-        passwordCriteriaBoolUpper === true ||
-        passwordCriteriaBoolLower === true) {
-        break;
-    }
-    else {
-        alert("enter a valid password");
-
-    }
-
-
-}
-
-generatePassword(passwordLength, passwordCriteriaBoolSpecial, passwordCriteriaBoolNumerical, passwordCriteriaBoolLower, passwordCriteriaBoolUpper);
 function generatePassword(length, specialCharBool, numericalCharBool, lowerCharBool, upperCharBool) {
     var passwordString;
     var passwordArr = [];
@@ -99,6 +103,7 @@ function generatePassword(length, specialCharBool, numericalCharBool, lowerCharB
     }
     passwordArr = passwordArr.join("");
    passwordString = passwordArr.toString(); 
+   document.getElementById("password").value=passwordString
    console.log(passwordString);
    alert("Your password is " + passwordString);
 
